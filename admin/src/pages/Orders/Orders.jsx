d448a42f472c5f9c5aa4af6ee86eea42c2070761
miankks <bilal.jan085@gmail.com>
2025-10-28 16:23:39 +0100

    dashboard orders showing now.

--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -1,8 +1,56 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 import './Orders.css'
+import axios from 'axios';
+import { toast } from 'react-toastify'
+import { assets } from '../../assets/assets';
 
-export const Orders = () => {
+export const Orders = ({url}) => {
+  const [orders, setOrders] = useState([]);
+  
+  const fetchAllOrders = async () => {
+    const response = await axios.get(url+'/api/order/list');
+    
+    if (response.data.success) {
+      setOrders(response.data.data);
+      console.log(response.data.data);
+      
+    } else {
+      toast.error("Error")
+    }
+  }
+
+  useEffect(() => {
+    fetchAllOrders();
+  },[])
+
   return (
-    <div>Orders</div>
+    <div className='order add'>
+      <h3>Order Page</h3>
+      <div className="order-list">
+        {orders.map((order, index) => (
+          <div className='order-item' key={index}>
+            <img src={assets.parcel_icon} alt="" />
+            <div>
+              <p className='order-item-car'>
+                {order.items.map((item, index) =>{
+                  if (index === order.items.length -1) {
+                    return item.name + " x" + item.quantity
+                  } else {
+                    return item.name + " x " +item.quantity + ", " 
+                  }
+                })}
+              </p>
+              <p className='order-item-name'>
+                {order.address.firstName+ " " + order.address.lastName}
+              </p>
+              <div className='order-item-address'>
+                {order.address.street+ ", "}
+              </div>
+              <p>{order.address.city+ " " + order.address.state+", " + order.address.country+ " " + order.address.zipcode}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
