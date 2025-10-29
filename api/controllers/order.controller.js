b9ccb07857436587f4d97aa2044b24fda3ecb89e
miankks <bilal.jan085@gmail.com>
2025-10-29 14:17:25 +0100

    changes fields in placeorder and set path from admin to client

--- api/controllers/order.controller.js
+++ api/controllers/order.controller.js
@@ -9,11 +9,10 @@ const placeOrder = async (req, res) => {
     const frontend_url = 'http://localhost:5173'
     try {
         // creating new order
-        const {userId, items, amount, address } = req.body;
+        const {userId, items, address } = req.body;
         const newOrder = new orderModel({
             userId: userId,
             items: items,
-            amount: amount,
             address: address
         })
 
@@ -24,38 +23,38 @@ const placeOrder = async (req, res) => {
         await userModel.findByIdAndUpdate(userId, {cartData: {}});
 
         // data for stripe payment
-        const line_items = items.map((item) => ({
-            price_data: {
-                currency: "sek",
-                product_data: {
-                    name: item.name
-                },
-                unit_amount: item.price * 100
-            },
-            quantity: item.quantity
-        }))
+        // const line_items = items.map((item) => ({
+        //     price_data: {
+        //         currency: "sek",
+        //         product_data: {
+        //             name: item.name
+        //         },
+        //         unit_amount: item.price * 100
+        //     },
+        //     quantity: item.quantity
+        // }))
 
         // delivery charges
-        line_items.push({
-            price_data:{
-                currency: "sek",
-                product_data: {
-                    name: "Delivery charges"
-                },
-                unit_amount: 2*100
-            },
-            quantity: 1
-        })
+        // line_items.push({
+        //     price_data:{
+        //         currency: "sek",
+        //         product_data: {
+        //             name: "Delivery charges"
+        //         },
+        //         unit_amount: 2*100
+        //     },
+        //     quantity: 1
+        // })
 
         // session for payment success and unsuccessful
-        const session = await stripe.checkout.sessions.create({
-            line_items: line_items,
-            mode: 'payment',
-            success_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
-            cancel_url: `${frontend_url}/verify?success=false&orderId=${newOrder._id}`,
-        })
+        // const session = await stripe.checkout.sessions.create({
+        //     line_items: line_items,
+        //     mode: 'payment',
+        //     success_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
+        //     cancel_url: `${frontend_url}/verify?success=false&orderId=${newOrder._id}`,
+        // })
 
-        res.json({success: true, session_url: session.url})
+        // res.json({success: true, session_url: session.url})
 
     } catch (error) {
         console.log(error);
@@ -63,25 +62,25 @@ const placeOrder = async (req, res) => {
     }
 }
 
-const verifyOrder = async (req, res) => {
-    const { orderId, success} = req.body
+// const verifyOrder = async (req, res) => {
+//     const { orderId, success} = req.body
 
-    try {
-        if (success=== "true") {
-            await orderModel.findByIdAndUpdate(orderId, {payment: true})
-            res.json({success: true, message: "Paid"})
-        } else {
-            await orderModel.findByIdAndDelete(ordderId)
-            res.json({success: false, message: "Not paid"})
-        }
-    } catch (error) {
+//     try {
+//         if (success=== "true") {
+//             await orderModel.findByIdAndUpdate(orderId, {payment: true})
+//             res.json({success: true, message: "Paid"})
+//         } else {
+//             await orderModel.findByIdAndDelete(ordderId)
+//             res.json({success: false, message: "Not paid"})
+//         }
+//     } catch (error) {
         
-        console.log(error);
-        res.json({success: false, message: "Error"})
+//         console.log(error);
+//         res.json({success: false, message: "Error"})
 
         
-    }
-}
+//     }
+// }
 
 // user orders for frontend
 const userOrders = async (req, res) => {
@@ -110,7 +109,7 @@ const listOrders = async (req, res) => {
 
 const updateStatus = async (req, res) => {
     try {
-        await orderModel.findByIdAndUpdate(req.body.orderId, {status: req.body.status});
+        await orderModel.findByIdAndUpdate(req.body.ordderId, {status: req.body.status});
         res.json({success: true, message: "Status updated"})
     } catch (error) {
         console.log(error);
@@ -118,4 +117,122 @@ const updateStatus = async (req, res) => {
         
     }
 }
-export {placeOrder, verifyOrder, userOrders, listOrders, updateStatus}
+export {placeOrder, userOrders, listOrders, updateStatus}
+
+
+// const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
+// // placing user order from frontend
+// const placeOrder = async (req, res) => {
+//     // link for frontend url
+//     const frontend_url = 'http://localhost:5173'
+//     try {
+//         // creating new order
+//         const {userId, items, amount, address } = req.body;
+//         const newOrder = new orderModel({
+//             userId: userId,
+//             items: items,
+//             amount: amount,
+//             address: address
+//         })
+
+//         // save new order in mongoDB
+//         await newOrder.save();
+
+//         // empty the user cart
+//         await userModel.findByIdAndUpdate(userId, {cartData: {}});
+
+//         // data for stripe payment
+//         const line_items = items.map((item) => ({
+//             price_data: {
+//                 currency: "sek",
+//                 product_data: {
+//                     name: item.name
+//                 },
+//                 unit_amount: item.price * 100
+//             },
+//             quantity: item.quantity
+//         }))
+
+//         // delivery charges
+//         line_items.push({
+//             price_data:{
+//                 currency: "sek",
+//                 product_data: {
+//                     name: "Delivery charges"
+//                 },
+//                 unit_amount: 2*100
+//             },
+//             quantity: 1
+//         })
+
+//         // session for payment success and unsuccessful
+//         const session = await stripe.checkout.sessions.create({
+//             line_items: line_items,
+//             mode: 'payment',
+//             success_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
+//             cancel_url: `${frontend_url}/verify?success=false&orderId=${newOrder._id}`,
+//         })
+
+//         res.json({success: true, session_url: session.url})
+
+//     } catch (error) {
+//         console.log(error);
+//         res.json({success: false, message: 'Error'})
+//     }
+// }
+
+// const verifyOrder = async (req, res) => {
+//     const { orderId, success} = req.body
+
+//     try {
+//         if (success=== "true") {
+//             await orderModel.findByIdAndUpdate(orderId, {payment: true})
+//             res.json({success: true, message: "Paid"})
+//         } else {
+//             await orderModel.findByIdAndDelete(ordderId)
+//             res.json({success: false, message: "Not paid"})
+//         }
+//     } catch (error) {
+        
+//         console.log(error);
+//         res.json({success: false, message: "Error"})
+
+        
+//     }
+// }
+
+// // user orders for frontend
+// const userOrders = async (req, res) => {
+//     try {
+//         const orders = await orderModel.find({userId: req.body.userId});
+//         res.json({success: true, data: orders})
+//     } catch (error) {
+//         console.log(error);
+//         res.json({success: false, message: "Error"})
+        
+//     }
+// }
+
+// // Listing orders for admin panel
+// const listOrders = async (req, res) => {
+//     try {
+//         const orders = await orderModel.find({});
+//         res.json({success: true, data: orders})
+//     } catch (error) {
+//         console.log(error);
+//         res.json({success: false, message: "Error"})
+//     }
+// }
+
+// // API for updating order status
+
+// const updateStatus = async (req, res) => {
+//     try {
+//         await orderModel.findByIdAndUpdate(req.body.ordderId, {status: req.body.status});
+//         res.json({success: true, message: "Status updated"})
+//     } catch (error) {
+//         console.log(error);
+//         res.json({success: false, message: "Error"})
+        
+//     }
+// }
--- client/src/pages/PlaceOrder/PlaceOrder.jsx
+++ client/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,23 +1,18 @@
-import React, { useContext, useEffect, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import './PlaceOrder.css';
 import { StoreContext } from '../../context/StoreContext';
 import axios from 'axios';
 
 const PlaceOrder = () => {
-    const { getTotalCartAmount, token, car_list, cartItems, url } = useContext(StoreContext);
+    const { getTotalCartAmount, token, car_list,removeFromCart, cartItems, url } = useContext(StoreContext);
     const navigate = useNavigate();
 
     const [data, setData] = useState({
-      firstName: '',
-      lastName: '',
+      fullName: '',
       email: '',
-      street: '',
-      city: '',
-      state: '',
-      zipcode: '',
-      country: '',
-      phone: ''
+      phone: '',
+      regnummer: ''
     })
 
     const onChangeHandler = (event) => {
@@ -44,10 +39,12 @@ const PlaceOrder = () => {
         items: orderItems,
         amount: getTotalCartAmount(),
       }
+      console.log(orderData);
+      
       let response = await axios.post(url+'/api/order/place', orderData, {headers: {token}})
       if (response.data.success) {
         const { session_url} = response.data;
-
+        navigate('/')
         // send user to session url
         window.location.replace(session_url);
       } else {
@@ -60,51 +57,53 @@ const PlaceOrder = () => {
       if (!token) {
         navigate('/cart')
       } 
-      // else if(getTotalCartAmount() === 0){
-      //   navigate('/cart')
-      // }
     }, [token])
 
   return (
     <form onSubmit={placeOrder} className='place-order'>
       <div className="place-order-left">
-        <p className="title">Delivery Information</p>
-        <div className="multi-fields">
-          <input  name='firstName' onChange={onChangeHandler} value={data.firstName} type="text" placeholder='First Name' />
-          <input  name='lastName' onChange={onChangeHandler} value={data.lastName} type="text" placeholder='Last Name' />
-        </div>
+        <p className="title">Fill in the information for Booking</p>
+        <label>Fullständigt namn</label>
+          <input required  name='fullName' onChange={onChangeHandler} value={data.fullName} type="text" placeholder='Full Name' />
+        <label>E-postadress</label>
         <input  name='email' onChange={onChangeHandler} value={data.email}  type="email" placeholder='Email address' />
-        <input  name='street' onChange={onChangeHandler} value={data.street} type="text" placeholder='Street' />
-        <div className="multi-fields">
-          <input  name='city' onChange={onChangeHandler} value={data.city} type="text" placeholder='City' />
-          <input  name='state' onChange={onChangeHandler} value={data.state} type="text" placeholder='State' />
-        </div>
-        <div className="multi-fields">
-          <input  name='zipcode' onChange={onChangeHandler} value={data.zipcode} type="text" placeholder='Zip code' />
-          <input  name='country' onChange={onChangeHandler} value={data.country} type="text" placeholder='Country' />
-        </div>
-        <input  name='phone' onChange={onChangeHandler} value={data.phone} type="text" placeholder='Phone' />
+        <label>Telefonnummer</label>
+        <input required name='phone' onChange={onChangeHandler} value={data.phone} type="text" placeholder='Phone' />
+        <label htmlFor="">Registreringsnummer</label>
+        <input required  name='regnummer' onChange={onChangeHandler} value={data.regnummer} type="text" placeholder='ABC123' />
       </div>
       <div className="place-order-right">
         <div className="cart-total">
-          <h2>Cart total</h2>
+          <h2>Väljat services</h2>
           <div>
-            <div className="cart-total-details">
-              <p>Subtotal</p>
-              <p>{getTotalCartAmount()}{" "}sek</p>
-            </div>
-            <hr />
-            {/* <div className="cart-total-details">
-              <p>Delivery Fee</p>
-              <p>{getTotalCartAmount() === 0 ? 0 : 50}{" "}sek</p>
-            </div> */}
-            <hr />
-            <div className="cart-total-details">
-              <b>Total</b>
-              <b>{getTotalCartAmount() === 0 ? 0 : getTotalCartAmount()}{" "}sek</b>
-            </div>
+            <div className="cart-items">
+        <div className="cart-items-title">
+          <p>Artiklar</p>
+          <p>Titel</p>
+          <p>Qvantitet</p>
+          <p>Ta bort</p>
+        </div>
+        <br />
+        <hr />
+        {car_list.map((item, index) => {
+          if (cartItems[item._id] > 0) {
+            return (
+              <div key={index}>
+                <div className="cart-items-title cart-items-item">
+                  <img src={url+'/images/'+item.image} alt="" />
+                  <p>{item.name}</p>
+                  {/* will show quantity of each product */}
+                  <p>{cartItems[item._id]}</p>
+                  <p className='cross' onClick={() => removeFromCart(item._id)}>x</p>
+                </div>
+                <hr />
+              </div>
+            )
+          }
+        })}
+      </div>
           </div>
-          <button type='submit'>Proceed to payment</button>
+          <button type='submit'>Complete Booking</button>
         </div>
       </div>
     </form>
@@ -112,3 +111,111 @@ const PlaceOrder = () => {
 }
 
 export default PlaceOrder
+
+
+// const PlaceOrder = () => {
+//     const { getTotalCartAmount, token, car_list, cartItems, url } = useContext(StoreContext);
+//     const navigate = useNavigate();
+
+//     const [data, setData] = useState({
+//       firstName: '',
+//       lastName: '',
+//       email: '',
+//       street: '',
+//       city: '',
+//       state: '',
+//       zipcode: '',
+//       country: '',
+//       phone: ''
+//     })
+
+//     const onChangeHandler = (event) => {
+//       const name= event.target.name
+//       const value = event.target.value
+
+//       setData(data => ({...data, [name]: value}))
+//     }
+
+//     const placeOrder = async (e) => {
+//       e.preventDefault();
+
+//       // before calling api structure data
+//       let orderItems = [];
+//       car_list.map((item)=> {
+//         if (cartItems[item._id] > 0) {
+//           let itemInfo = item;
+//           itemInfo['quantity'] = cartItems[item._id];
+//           orderItems.push(itemInfo)
+//         }
+//       })
+//       let orderData = {
+//         address: data,
+//         items: orderItems,
+//         amount: getTotalCartAmount(),
+//       }
+//       let response = await axios.post(url+'/api/order/place', orderData, {headers: {token}})
+//       if (response.data.success) {
+//         const { session_url} = response.data;
+
+//         // send user to session url
+//         window.location.replace(session_url);
+//       } else {
+//         alert('Elert error')
+//       }
+      
+//     }
+
+//     useEffect(() => {
+//       if (!token) {
+//         navigate('/cart')
+//       } 
+//       // else if(getTotalCartAmount() === 0){
+//       //   navigate('/cart')
+//       // }
+//     }, [token])
+
+//   return (
+//     <form onSubmit={placeOrder} className='place-order'>
+//       <div className="place-order-left">
+//         <p className="title">Delivery Information</p>
+//         <div className="multi-fields">
+//           <input  name='firstName' onChange={onChangeHandler} value={data.firstName} type="text" placeholder='First Name' />
+//           <input  name='lastName' onChange={onChangeHandler} value={data.lastName} type="text" placeholder='Last Name' />
+//         </div>
+//         <input  name='email' onChange={onChangeHandler} value={data.email}  type="email" placeholder='Email address' />
+//         <input  name='street' onChange={onChangeHandler} value={data.street} type="text" placeholder='Street' />
+//         <div className="multi-fields">
+//           <input  name='city' onChange={onChangeHandler} value={data.city} type="text" placeholder='City' />
+//           <input  name='state' onChange={onChangeHandler} value={data.state} type="text" placeholder='State' />
+//         </div>
+//         <div className="multi-fields">
+//           <input  name='zipcode' onChange={onChangeHandler} value={data.zipcode} type="text" placeholder='Zip code' />
+//           <input  name='country' onChange={onChangeHandler} value={data.country} type="text" placeholder='Country' />
+//         </div>
+//         <input  name='phone' onChange={onChangeHandler} value={data.phone} type="text" placeholder='Phone' />
+//       </div>
+//       <div className="place-order-right">
+//         <div className="cart-total">
+//           <h2>Cart total</h2>
+//           <div>
+//             <div className="cart-total-details">
+//               <p>Subtotal</p>
+//               <p>{getTotalCartAmount()}{" "}sek</p>
+//             </div>
+//             <hr />
+//             {/* <div className="cart-total-details">
+//               <p>Delivery Fee</p>
+//               <p>{getTotalCartAmount() === 0 ? 0 : 50}{" "}sek</p>
+//             </div> */}
+//             <hr />
+//             <div className="cart-total-details">
+//               <b>Total</b>
+//               <b>{getTotalCartAmount() === 0 ? 0 : getTotalCartAmount()}{" "}sek</b>
+//             </div>
+//           </div>
+//           <button type='submit'>Proceed to payment</button>
+//         </div>
+//       </div>
+//     </form>
+//   )
+// }
