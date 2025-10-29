4e55f5a05537503eb9f7d8fddd27b602c9f6ec5f
miankks <bilal.jan085@gmail.com>
2025-10-29 09:09:04 +0100

    order status API functional

--- api/controllers/order.controller.js
+++ api/controllers/order.controller.js
@@ -106,4 +106,16 @@ const listOrders = async (req, res) => {
     }
 }
 
-export {placeOrder, verifyOrder, userOrders, listOrders}
+// API for updating order status
+
+const updateStatus = async (req, res) => {
+    try {
+        await orderModel.findByIdAndUpdate(req.body.orderId, {status: req.body.status});
+        res.json({success: true, message: "Status updated"})
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: "Error"})
+        
+    }
+}
+export {placeOrder, verifyOrder, userOrders, listOrders, updateStatus}
--- api/routes/order.route.js
+++ api/routes/order.route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import authMiddleware from '../middleware/auth.js'
-import { listOrders, placeOrder, userOrders, verifyOrder } from '../controllers/order.controller.js';
+import { listOrders, placeOrder, updateStatus, userOrders, verifyOrder } from '../controllers/order.controller.js';
 
 const orderRouter = express.Router();
 
@@ -8,5 +8,6 @@ orderRouter.post("/place", authMiddleware, placeOrder);
 orderRouter.post("/verify", verifyOrder);
 orderRouter.post("/userorders", authMiddleware, userOrders);
 orderRouter.get("/list", listOrders);
+orderRouter.post("/status", updateStatus);
 
 export default orderRouter;
