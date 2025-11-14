42cc9f7563d2c67c500247b4ad419982ac186a59
miankks <bilal.jan085@gmail.com>
2025-11-14 10:13:27 +0100

    email sending to only registered account in resend

--- api/controllers/email.controller.js
+++ api/controllers/email.controller.js
@@ -1,53 +1,47 @@
 import nodemailer from 'nodemailer';
 import bodyParser from 'body-parser';
+import { Resend } from 'resend';
 
 
+const resend = new Resend(process.env.RESEND_API_KEY);
 const sendEmail = async (req, res) => {
-    const {username, email, subject, regnumber } = req.body
-    console.log(req.body);
+   console.log("🟡 sendEmail called");
+  console.log("🟡 body:", req.body);
+  try {
+    const {name, email, subject, regnumber } = req.body
+    console.log("🟡 Preparing to send email...");
     
-    try {   
-        // create mail transporter
-        const transpoter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: 'miankks85@gmail.com',
-                pass: 'qwerty123', // use app pasword if gmail
-            }
-        })
-    // email content (send back to user)
-    const mailOptions = {
-        from: '"Exobil" techbilal85@gmail.com',
-        to: email,
-        subject: `Thanks for contacting us, ${username || 'friend'}`,
-        html: `
-            <h2>Hello ${username || ''}!</h2>
-            <p>Thanks for reaching out</p>
-            <p>We received your message:</p>
-            <p>${subject}</p>
-            <blockquote>${regnumber || 'No message provided'}</blockquote>
-            <p>We'll get back to you soon.</p>
-            <p>Exobil</p>
-        `,
-    }
+            const {data, error} = await resend.emails.send({
+                    from: 'Exobil <onboarding@resend.dev>',
+                    to: email,
+                    subject: `Thanks for contacting us, ${name || 'friend'}`,
+                    html: `
+                        <h2>Hello ${name || ''}!</h2>
+                        <p>Thanks for reaching out</p>
+                        <p>We received your message:</p>
+                        <p>${subject}</p>
+                        <blockquote>${regnumber || 'No message provided'}</blockquote>
+                        <p>We'll get back to you soon.</p>
+                        <p>Exobil</p>
+                    `,
+            })
+            console.log("🟢 SEND RESULT:");
+            console.log("data →", data);
+            console.log("error →", error);
 
-    
-    // send email
-    await transpoter.sendEmail(mailOptions);
-    res.status(200).json({success: true, message: 'Email sent to user'});
-    } catch (error) {
-        console.error(500).json({success: false, error: 'Failed to send email' });
-        
-    }
+             if (error) {
+                console.error({ error });
+                return res.status(400).json({ error: error.message });
+  }
+
+  console.log(data);
+    return res.status(200).json({ message: "Email sent successfully", data });
+
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Server error" });
+  }
+  
 }
 
 export {sendEmail}
-
-  // create mail transporter
-        // const transpoter = nodemailer.createTransport({
-        //     service: 'gmail',
-        //     auth: {
-        //         user: 'miankks85@gmail.com',
-        //         pass: 'qwerty123', // use app pasword if gmail
-        //     }
-        // })
--- api/models/email.model.js
+++ api/models/email.model.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const emailSchema = new mongoose.Schema({
 
-     username: {
+     name: {
         type: String
     },
     email: {
