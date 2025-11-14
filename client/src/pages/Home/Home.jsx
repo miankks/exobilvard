1ca92f532db7105e8f1a80f5346a69276de682f8
Bilal Jan <bilal.jan085@gmail.com>
2025-11-14 14:38:38 +0100

    remove package lock

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -3,8 +3,6 @@ import './Home.css';
 import Header from '../../components/Header/Header';
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu';
 import CarDisplay from '../../components/CarDisplay/CarDisplay';
-import AppDownload from '../../components/AppDownload/AppDownload';
-import BackToTop from '../../components/BackToTop/BackToTop';
 
 const Home = () => {
   const [category, setCategory] = useState("All");
@@ -12,10 +10,8 @@ const Home = () => {
   return (
     <div>
         <Header />
-        {/* <BackToTop /> */}
         <ExploreMenu category={category} setCategory={setCategory}/>
         <CarDisplay category={category}/>
-        {/* <AppDownload /> */}
     </div>
   )
 }
