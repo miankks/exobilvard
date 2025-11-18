d92bf01e2cfb58beb12ce6c890b859b5779c219d
Bilal Jan <bilal.jan085@gmail.com>
2025-11-18 14:02:41 +0100

    popup for delete item and not directly delrte

--- admin/src/pages/RejectedOrders/RejectedOrders.jsx
+++ admin/src/pages/RejectedOrders/RejectedOrders.jsx
@@ -7,6 +7,8 @@ import { assets } from '../../assets/assets';
 const RejectedOrders = ({url}) => {
   const [orders, setOrders] = useState([]);
   const [selectedStatuses, setSelectedStatuses] = useState({});
+  const [confirmDeleteId, setConfirmDeleteId] = useState(null);
+
   
   const fetchAllOrders = async () => {
     const response = await axios.get(url+'/api/order/rejectedorders');
@@ -108,15 +110,46 @@ const RejectedOrders = ({url}) => {
               Skicka
             </button>
             <div className='delete-btn'>
-                <button type='submit' className='add-btn' onClick={() =>
-                      deleteHandler(order._id)
-                }>
+                <button type='submit' className='max-w-60 px-4 py-3 bg-red-500 text-white rounded-lg hover:bg-red-600 cursor-pointer m-10'
+                    onClick={() => setConfirmDeleteId(order._id)}
+                  >
                   Delete
                 </button>
             </div>
           </div>
         ))}
       </div>
+        {confirmDeleteId && (
+          <div className="fixed inset-0 bg-black bg-opacity-40 flex items-center justify-center animate-fadeIn">
+            <div className="bg-white p-6 rounded-2xl shadow-2xl w-80 animate-slideUp">
+              <h2 className="text-xl font-semibold text-gray-800 mb-3">
+                Delete Item?
+              </h2>
+              <p className="text-gray-600 mb-6">
+                Are you sure you want to delete this item? This action cannot be undone.
+              </p>
+
+              <div className="flex justify-end gap-3">
+                <button
+                  className="px-4 py-2 rounded-lg bg-gray-200 hover:bg-gray-300"
+                  onClick={() => setConfirmDeleteId(null)}
+                >
+                  Cancel
+                </button>
+
+                <button
+                  className="px-4 py-2 rounded-lg bg-red-500 text-white hover:bg-red-600"
+                  onClick={() => {
+                    deleteHandler(confirmDeleteId);
+                    setConfirmDeleteId(null);
+                  }}
+                >
+                  Delete
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
     </div>
   )
 }
