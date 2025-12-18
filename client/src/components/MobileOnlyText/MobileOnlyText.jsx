520894d790646b74b7419253c629071237314392
Bilal Jan <bilal.jan085@gmail.com>
2025-12-18 13:12:32 +0100

    mobile version categories working

--- client/src/components/MobileOnlyText/MobileOnlyText.jsx
+++ client/src/components/MobileOnlyText/MobileOnlyText.jsx
@@ -1,5 +1,7 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import './MobileOnlyText.css';
+import { menu_list } from '../../assets/assets'
+import { CarContext } from '../../context/CarContext';
 
 const services = [
   'Avgaser',
@@ -11,40 +13,50 @@ const services = [
   'Tillbehör & extrautrustning'
 ];
 
-const MobileOnlyText = ({ selectedService, setSelectedService  }) => {
-  const [expanded, setExpanded] = useState(false);
+const MobileOnlyText = ({ selectedService, setSelectedService, category  }) => {
+  const [expandedIndex, setExpandedIndex] = useState(null);
+  const { car_list } = useContext(CarContext);
 
-  const handleToggle = () => {
-    setExpanded(prev => !prev);
+  const handleToggle = (index) => {
+    setExpandedIndex(prev => (prev === index ? null : index));
   };
-
+  
+  console.log(car_list);
+  
   const handleSelect = (service) => {
     setCategory(service); // Update selected service
-    setExpanded(false);   // Close accordion after selection
+    setExpanded(null);   // Close accordion after selection
   };
+
 
    return (
     <div className="mobile-text-accordion">
-      <div className={`accordion-card ${expanded ? 'accordion-card-expanded' : ''}`} onClick={handleToggle}>
-        <div className="accordion-header">
-          <p>{selectedService || 'Välj en tjänst'}</p>
-          <span className="accordion-toggle">{expanded ? '-' : '+'}</span>
-        </div>
-
-        {expanded && (
-          <div className="accordion-body">
-            {services.map((service, idx) => (
-              <p
-                key={idx}
-                className="accordion-item"
-                onClick={() => handleSelect(service)}
-              >
-                {service} <span>+</span>
-              </p>
+            {menu_list.map((item, index) => (
+                <div key={index} 
+                    className={`accordion-card ${expandedIndex === index ? 'accordion-card-expanded' : ''}`} 
+                >
+                    <div className='accordion-header' onClick={() => handleToggle(index)}>
+                        <p>{item.menu_name || 'Välj en tjänst'}</p>
+                        <span className="accordion-toggle">{expandedIndex === index ? '-' : '+'}</span>
+                    </div>
+                    {expandedIndex === index && (
+                    <div className="accordion-body">
+                        {car_list.map((service, idx) => {
+                             if (item.menu_name === service.category) {
+                                return (
+                                    <p
+                                        key={idx}
+                                        className="accordion-item"
+                                        onClick={() => handleSelect(service)}
+                                    >
+                                        {service.name} <span>+</span>
+                                    </p>
+                                )}
+                    })}
+                    </div>
+                    )}
+                </div>
             ))}
-          </div>
-        )}
-      </div>
     </div>
   );
 };
