e1d5dd3b91499da0089b13e9a855cc76d10786d1
Bilal Jan <bilal.jan085@gmail.com>
2025-12-29 12:00:09 +0100

    text add

--- client/src/components/CarTyre/CarTyre.jsx
+++ client/src/components/CarTyre/CarTyre.jsx
@@ -1,41 +1,50 @@
-import React from "react";
 import "./CarTyre.css";
 
 const CarTyre = () => {
   return (
-    <div className="tyre-container">
-      {/* RED LINES (behind wheel) */}
-      <div className="lines">
-        <span className="line line1">
-          <div className="card">Säker & trygg hantering</div>
-        </span>
-        <span className="line line2">
-          <div className="card">Professionell montering</div>
-        </span>
-        <span className="line line3">
-          <div className="card">Däckförvaring tillgänglig</div>
-        </span>
-        <span className="line line4">
-          <div className="card">Balansering & Inställning</div>
-        </span>
+    <div className="tyre-wrapper">
+      {/* LEFT DIV */}
+      <div className="left-info">
+        <h3>Säsongsservice året runt</h3>
+        <ul>
+          <li>Sommardäck & Vinterdäck</li>
+          <li>Balansering & Inställning</li>
+          <li>Däckförvaring tillgänglig</li>
+          <li>Professionell montering</li>
+          <li>Säker & trygg hantering</li>
+        </ul>
       </div>
-      <div className="wheel">
-        <div className="rim">
-          <div className="spoke"></div>
-          <div className="spoke"></div>
-          <div className="spoke"></div>
-          <div className="spoke"></div>
-          <div className="spoke"></div>
-          <div className="spoke"></div>
-          <div className="spoke"></div>
-          <div className="spoke"></div>
-          <div className="hub"></div>
+
+      {/* RIGHT TYRE */}
+      <div className="tyre-container small">
+        <div className="lines">
+          <span className="line line1">
+            <div className="card">Säker & trygg hantering</div>
+          </span>
+          <span className="line line2">
+            <div className="card">Professionell montering</div>
+          </span>
+          <span className="line line3">
+            <div className="card">Däckförvaring tillgänglig</div>
+          </span>
+          <span className="line line4">
+            <div className="card">Balansering & Inställning</div>
+          </span>
+        </div>
+        <div className="wheel">
+          <div className="rim">
+            <div className="spoke"></div>
+            <div className="spoke"></div>
+            <div className="spoke"></div>
+            <div className="spoke"></div>
+            <div className="spoke"></div>
+            <div className="spoke"></div>
+            <div className="spoke"></div>
+            <div className="spoke"></div>
+            <div className="hub"></div>
+          </div>
         </div>
       </div>
-      {/* <div className="text-1">
-        <h5>Säker & trygg hantering</h5>
-        <p>Professionell hantering av dina däck med försiktighet och omsorg.</p>
-      </div> */}
     </div>
   );
 };
