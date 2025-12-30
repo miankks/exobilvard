5caddf3e0acad4540a7058a640dd55ea16f56229
Bilal Jan <bilal.jan085@gmail.com>
2025-12-30 15:07:24 +0100

    vite test configure in admin

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,12 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  server:{
+  server: {
     port: 5000,
-    historyApiFallback: true
+    historyApiFallback: true,
   },
-  plugins: [react(), tailwindcss(),],
-})
+  plugins: [react(), tailwindcss()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.js",
+  },
+});
