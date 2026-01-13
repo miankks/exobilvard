e0eebfa120bceb8f1521c6611f26a717008a9658
Bilal Jan <bilal.jan085@gmail.com>
2026-01-13 14:06:01 +0100

    password show hide functionality

--- admin/src/pages/AdminLogin/AdminLogin.jsx
+++ admin/src/pages/AdminLogin/AdminLogin.jsx
@@ -2,12 +2,15 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
 import "./AdminLogin.css";
+import { BiHide, BiShow } from "react-icons/bi";
 
 const AdminLogin = () => {
   const { login } = useAuth();
   const navigate = useNavigate();
   const [formData, setFormData] = useState({ email: "", password: "" });
   const [message, setMessage] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [changePasswordIcon, setChangePasswordIcon] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -18,6 +21,11 @@ const AdminLogin = () => {
     } else {
       setMessage(result.message);
     }
+  };
+
+  const handleShowPassword = () => {
+    setShowPassword(!showPassword);
+    setChangePasswordIcon(!changePasswordIcon);
   };
 
   return (
@@ -32,16 +40,22 @@ const AdminLogin = () => {
           onChange={(e) => setFormData({ ...formData, email: e.target.value })}
           required
         />
-
-        <input
-          type="password"
-          placeholder="Password"
-          value={formData.password}
-          onChange={(e) =>
-            setFormData({ ...formData, password: e.target.value })
-          }
-          required
-        />
+        <div className="password-wrapper">
+          <input
+            type={showPassword ? "text" : "password"}
+            placeholder="Password"
+            value={formData.password}
+            onChange={(e) =>
+              setFormData({ ...formData, password: e.target.value })
+            }
+            required
+          />
+          {changePasswordIcon ? (
+            <BiShow onClick={handleShowPassword} className="show-password" />
+          ) : (
+            <BiHide onClick={handleShowPassword} className="show-password" />
+          )}
+        </div>
 
         <button type="submit">Sign In</button>
         {message && <p className="message">{message}</p>}
