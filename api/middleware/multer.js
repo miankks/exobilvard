1b793b4b3e769f6e4c3c2a73b5223851bbbf728d
Bilal Jan <bilal.jan085@gmail.com>
2026-01-13 13:40:53 +0100

    multer set for local storage

--- api/middleware/multer.js
+++ api/middleware/multer.js
@@ -1,38 +1,22 @@
-// import multer from "multer";
-// import { storage } from "../config/cloudinary.js";
-
-// const upload = multer({ storage });
-
-// export default upload;
-
 import multer from "multer";
 import fs from "fs";
 import path from "path";
 
-let storage;
+const uploadPath = path.join(process.cwd(), "uploads");
 
-if (process.env.NODE_ENV === "production") {
-  // Serverless: Use memory storage
-
-  storage = multer.memoryStorage();
-} else {
-  // save to local
-  const uploadPath = path.join(process.cwd(), "uploads");
-
-  // make sure folder exista locally
-  if (!fs.existsSync(uploadPath)) {
-    fs.mkdirSync(uploadPath, { recursive: true });
-  }
-
-  storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, uploadPath);
-    },
-    filename: (req, file, cb) => {
-      cb(null, `${Date.now()}-${file.originalname}`);
-    },
-  });
+// make sure folder exists locally
+if (!fs.existsSync(uploadPath)) {
+  fs.mkdirSync(uploadPath, { recursive: true });
 }
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, uploadPath);
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
 
 const upload = multer({ storage });
 
